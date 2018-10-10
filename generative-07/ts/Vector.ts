060fe1d28bc4e7d9ccea9bbe765cadb3887ae83a
Martin Itterheim <itterheim.martin@gmail.com>
2018-10-10 19:26:04 +0200

    remove

--- generative-07/ts/Vector.ts
+++
@@ -1,8 +0,0 @@
-export class Vector {
-    constructor (public x: number, public y: number) { }
-
-    public add (vector: Vector) {
-        this.x += vector.x;
-        this.y += vector.y;
-    }
-}
